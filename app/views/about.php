bded35294a31ebe36bb58d41d97375feb7c374ee
stml <james@shorttermmemoryloss.com>
2016-01-08 16:58:56 +0200

    About page & CSS

--- app/views/about.php
+++ app/views/about.php
@@ -1,16 +1,13 @@
 <div id="content">
   <h2>You are not on the internet</h2>
   <p>You have connected to a wifi network that leads only to this website. If you're looking for the internet, disconnect from the hello.box network and look elsewhere.</p>
-  <p><?php echo _('You are connected to a wifi router that’s been hacked to connect you to other people nearby. You are “offline” in relation to the rest of the Internet, but “online” in a way that connects you to others within range of the wifi signal.'); ?></p>
-  <h2 class="top"><?php echo _('About this box'); ?></h2>
-  <p><?php echo _('Occupy.here is a free, anonymous, and uncensored resource for you and your neighbors. You can post messages, share media files, and occupy the virtual space here as you wish.'); ?></p>
-  <!--<h2><?php echo _('Occupy.here is a network'); ?></h2>
-  <p><?php echo _('While this wifi router is not directly wired to any upstream network provider, it’s also not entirely isolated. There is another way that data can be transmitted to other occupy.here routers. The router’s local database has been quietly backed up onto your browser. Upon connecting to another Occupy.here router, the data you’ve carried with you will be synced and merged into that node’s database.'); ?></p>
-  <p><?php echo _('Messages can propagate across the network, but your device is the conduit instead of an upstream ISP. The network of Occupy.here routers is very loosely connected, data moves slowly using a process that resembles insects polinating flowers.'); ?></p>-->
-  <h2><?php echo _('Named, anonymous accounts'); ?></h2>
-  <p><?php echo _('On Occupy.here you post anonymously like with weblog comments. You have a named account, but there is no password or verification process. Content you post here may be copied to other Occupy.here wifi routers.'); ?></p>
-  <h2><?php echo _('Help us build Occupy.here'); ?></h2>
-  <p><?php echo _('Occupy.here is all-volunteer'); ?>, <a href="https://github.com/dphiffer/occupy.here" class="off-site"><?php echo _('open source'); ?></a>, <?php echo _('and'); ?> <a href="http://www.fsf.org/" class="off-site"><?php echo _('Free Software'); ?></a>. <a href="http://phiffer.org/" class="off-site">Dan Phiffer</a> <?php echo _('is the founder and lead developer, with contributions from'); ?> <a href="http://boxysean.com/" class="off-site">Sean McIntyre</a> <?php echo _('and GitHub user'); ?> <a href="https://github.com/Phaeilo" class="off-site">Phaeilo</a>. <?php echo _('In 2012 the project was fortunate to receive a commissions grant from'); ?> <a href="http://rhizome.org/" class="off-site">Rhizome</a>.</p>
-  <h2><?php echo _('Setting up your own router'); ?></h2>
-  <p><?php echo _('Occupy.here is designed to be replicated. You can install the custom OpenWRT image on your own router. Visit'); ?> <a href="http://occupyhere.org" class="off-site">occupyhere.org</a> <?php echo _('for the step-by-step installation guide.'); ?></p>
+  
+  <h2>Hello, box</h2>
+  <p>This is <strong>hello.box</strong>, a small, portable, personal backchannel. It's a wifi router carried around by someone wherever they go. They can show you stuff, and you can leave them messages, and talk to other people who are also nearby. That's it.</p>
+  <p>hello.box was developed by James Bridle. Find out more at <strong>http://booktwo.org</strong>. (No, that's not a link. You'll need the internet to do that!)</p>
+  
+  <h2>Thanks</h2>
+  <p>hello.box is based on <strong>occupy.here</strong>, a free, anonymous, local forum inspired by Occupy Wall Street and developed by Dan Phiffer. Find out more at <strong>http://occupyhere.org</strong> (Still not a link).</p>
+  <p>hello.box was also inspired by Aram Bartholl's OFFLINE ART project, showing offline net art projects through dedicated wifi points in a gallery space. See <strong>http://www.datenform.de</strong> for more info.</p>
+  
 </div>
